1405bbe12bd65894fb792354f746aa4c9c74052f
Ulyana <u.katrichko@yandex.ru>
2018-11-10 21:51:09 +0300

    finish 5.2

--- 1 sem/5 week/2 problem/Problem_2/Problem_2.cpp
+++ 1 sem/5 week/2 problem/Problem_2/Problem_2.cpp
@@ -1,7 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
-#include <conio.h>//!!!!!!!!!!!!!!!!!
+
 struct Element
 {
 	int data = 0;
@@ -12,7 +12,7 @@ struct List
 {
 	Element* head = nullptr;
 	Element* toKill = nullptr;
-}list;
+};
 
 void addToList(int nuberOfWarriors, List &list)
 {
@@ -27,11 +27,20 @@ void addToList(int nuberOfWarriors, List &list)
 	elemToAdd->next = list.head;
 }
 
+Element* pointerOnPrevious(List &list, Element* current)
+{
+	Element* previous = list.head;
+	while (previous->next != current)
+	{
+		previous = previous->next;
+	}
+	return previous;
+}
 
-/*int whoSurvive(int victimNumber)
+int whoSurvive(int victimNumber, List list)
 {
 	list.toKill = list.head;
-	while (list.head != list.tail)
+	while (list.head->next != list.head)
 	{
 		for (int i = 1; i < victimNumber; ++i)
 		{
@@ -41,27 +50,22 @@ void addToList(int nuberOfWarriors, List &list)
 		{
 			list.head = list.head->next;
 		}
-		if (list.toKill == list.tail) 
-		{
-			list.tail = list.tail->prev;
-		}
-		(list.toKill->prev)->next = list.toKill->next;
-		(list.toKill->next)->prev = list.toKill->prev;
+		pointerOnPrevious(list, list.toKill)->next = list.toKill->next;
 		Element *temp = list.toKill;
 		list.toKill = list.toKill->next;
 		delete temp;
 	}
-	int const survivorNumber = list.tail->data;
-	delete list.tail;
+	int const survivorNumber = list.head->data;
+	delete list.head;
 	list.head = nullptr;
-	list.tail = nullptr;
 	return survivorNumber;
 }
 
 int testFunc(int numberOfWarriors, int victimNumber)
 {
-	addToList(numberOfWarriors, list);
-	return whoSurvive(victimNumber);
+	List listTest;
+	addToList(numberOfWarriors, listTest);
+	return whoSurvive(victimNumber, listTest);
 }
 
 bool test()
@@ -70,16 +74,16 @@ bool test()
 		testFunc(4, 2) == 1 && testFunc(1, 5) == 1 && testFunc(5, 7) == 4 && testFunc(10, 2) == 5 &&
 		testFunc(7, 2) == 7 && testFunc(2, 2) == 1 && testFunc(2, 3) == 2);
 }
-*/
+
 int main()
 {
-	/*if (!test())
+	if (!test())
 	{
 		printf("Program doesn't work :(");
 		return -1;
 	}
 	else
-	{*/
+	{
 		int numberOfWarriors = 0;
 		int victimNumber = 0;
 
@@ -87,11 +91,10 @@ int main()
 		scanf("%d", &numberOfWarriors);
 		printf("Enter the number of victim:   ");
 		scanf("%d", &victimNumber);
-
+		
+		List list;
 		addToList(numberOfWarriors, list);
-		//printf("Number of survivivor:   %d", whoSurvive(victimNumber));
-	//}
-
-	_getch();//!!!!!!!!!
+		printf("Number of survivivor:   %d", whoSurvive(victimNumber, list));
+	}
 	return 0;
 }
