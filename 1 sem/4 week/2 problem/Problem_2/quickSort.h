3f4531ecba58e632f1b02729ce2cb89d811e4c3b
Ulyana <u.katrichko@yandex.ru>
2018-11-16 14:53:45 +0300

    delete

--- 1 sem/4 week/2 problem/Problem_2/quickSort.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-void swap(int &a, int&b);
-int partition(int* arr, int left, int right);
-void insertionSort(int* arr, int left, int right);
-void quickSort(int* arr, int left, int right);
