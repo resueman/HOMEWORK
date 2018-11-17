07b25781fb809b5cfb52d5059152b06e2fe5f6f4
Ulyana <u.katrichko@yandex.ru>
2018-11-17 21:15:14 +0300

    add 6.1 problem solution

---
+++ 1 sem/6 week/1 problem/1 problem/stack.h
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace stack
+{
+	struct Stack;
+	//Creates a new instance of stack.
+	Stack *createStack();
+	//Add element to stack
+	void push(Stack *stack, char data);
+	//This function pop element from stack and return it's value 
+	int pop(Stack *stack);
+	//This function return true if stack is empty
+	bool isEmpty(Stack *stack);
+	//Delete stack
+	void deleteStack(Stack &stack);
+}
