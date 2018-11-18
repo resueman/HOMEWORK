00db61ba2f2761c2b8bf4792dd44fa2ed7984175
Ulyana <u.katrichko@yandex.ru>
2018-11-18 20:59:59 +0300

    add comments

--- 1 sem/7 week/1 problem/problem 1/print.h
+++ 1 sem/7 week/1 problem/problem 1/print.h
@@ -2,6 +2,7 @@
 
 #include "tree.h"
 #include <string>
-
+///print set in descend order
 void printDescendingOrder(Set* set, Node* node, std::string &resultStr);
+///print set in ascend order
 void printAscendingOrder(Set* set, Node* node, std::string &resultStr);
--- 1 sem/7 week/1 problem/problem 1/tree.h
+++ 1 sem/7 week/1 problem/problem 1/tree.h
@@ -13,13 +13,22 @@ struct Node
 	Node* leftChild = nullptr;
 	Node* rightChild = nullptr;
 };
-
+///function that creates set
 Set* createSet();
+///delete set
 void deleteSet(Set* set, Node* node);
+///check if set already contains element, is it empty
 bool addToSet(Set* set, int const  data);
-bool removeFromSet(Set* set, int const data);
-bool exists(Set* set, int const data);
-bool isEmpty(Set* set);
-void removeNode(Node*& current, int const data);
-int maximum(Node* current);
+///add element
 void addNode(Node* node, int const data);
+///check if set is empty, if it contains element we want to delete
+bool removeFromSet(Set* set, int const data);
+///delete element by it's value
+void removeNode(Node*& current, int const data);
+///check if element exists in a set
+bool exists(Set* set, int const data);
+///check if set is empty
+bool isEmpty(Set* set);
+///search max element in a set to put it on deleted element place
+int maximum(Node* current);
+
